89b10be5bdc857e3aa863c59ac886bd4e5c28de0
Shen yu <yu.shen@hpe.com>
2016-11-25 09:40:35 +0000

    ut

--- test/user-rs.test.js
+++ test/user-rs.test.js
@@ -4,13 +4,10 @@ const assert = require('chai').assert
   
 
 describe('User CRUD', () => {
-    let newId;
     it('findAll', (done) => {
         request.get('/api/users')
             .expect(res => {
-                assert.isNotNull(res.body.id);
                 assert.equal(res.body.length, 3);
-                newId = res.body.id;
             })
             .expect(200,done);
     });
